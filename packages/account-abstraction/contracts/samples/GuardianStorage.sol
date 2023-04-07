ef1a1d90ef206dcbe22f9679e1064d7be5c7f647
skyh24 <skyhighfeng@gmail.com>
2023-04-07 20:46:20 +0800

    privateguardian

---
+++ packages/account-abstraction/contracts/samples/GuardianStorage.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
+
+import "./interfaces/IUpdateGuardianVerifier.sol";
+import "./interfaces/ISocialRecoveryVerifier.sol";
+import "./interfaces/IPoseidonHasher.sol";
+
+library GuardianStorage {
+  using EnumerableMap for EnumerableMap.UintToUintMap;
+
+  bytes32 private constant GUARDIANS_SLOT = keccak256("accountjs.contracts.GuardiansStorage");
+
+  uint8 constant MAX_GUARDIANS_NUM = 7;
+
+  struct Layout {
+    // merkle root
+    uint root;
+
+    // number of guardians
+    uint num_guardians;
+
+    // current vote number on change owner
+    uint cur_vote;
+
+    // vote threshold
+    uint vote_threshold;
+
+    // guardians
+    uint[MAX_GUARDIANS_NUM] guardians;
+
+    // record of in progress voter
+    EnumerableMap.UintToUintMap recover_nullifier_set;
+
+    // update guardian verifier contract
+    IUpdateGuardianVerifier updateGuardianVerifier;
+
+    // social recover verifier contract
+    ISocialRecoveryVerifier socialRecoveryVerifier;
+
+    // poseidon hasher verifier contract
+    IPoseidonHasher hasher;
+  }
+
+  function layout() internal pure returns (Layout storage l) {
+    bytes32 slot = GUARDIANS_SLOT;
+    assembly {
+      l.slot := slot
+    }
+  }
+
+  function initialize(
+    GuardianStorage.Layout storage l,
+    uint[] memory guardians,
+    uint vote_threshold,
+    uint root,
+    address updateGuardianVerifierAddress,
+    address socialRecoveryVerifierAddress,
+    address poseidonContractAddress
+  ) internal {
+    require(guardians.length <= MAX_GUARDIANS_NUM, "Too many guardians");
+    require(guardians.length / 2  < vote_threshold, "Vote threshold must be larger than half of total guardians");
+
+    for(uint i = 0; i< guardians.length; i++) {
+      l.guardians[i] = guardians[i];
+    }
+    l.num_guardians = guardians.length;
+    l.vote_threshold = vote_threshold;
+    l.root = root;
+    l.updateGuardianVerifier = IUpdateGuardianVerifier(updateGuardianVerifierAddress);
+    l.socialRecoveryVerifier = ISocialRecoveryVerifier(socialRecoveryVerifierAddress);
+    l.hasher = IPoseidonHasher(poseidonContractAddress);
+  }
+
+  function updateGuardian(
+    GuardianStorage.Layout storage l,
+    uint[2] memory a,
+    uint[2][2] memory b,
+    uint[2] memory c,
+    uint[6] memory input //newRoot, oldRoot, indexOfGuardian, oldPubKey[0], oldPubKey[1], newPubKey
+  ) external returns (bool) {
+    // check root
+    require(l.root == input[1], "Wrong merkel root");
+
+    // check proof
+    if(l.updateGuardianVerifier.verifyProof(a, b, c, input)) {
+      // update guardian
+      l.guardians[input[2] - 1] = input[5];
+
+      // update root
+      l.root = input[0];
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function recover(
+    GuardianStorage.Layout storage l,
+    uint[2] memory a,
+    uint[2][2] memory b,
+    uint[2] memory c,
+    uint[3] memory input, //nullifier, hashOfNewOwner, merkleRoot
+    address newOwner
+  ) external returns (bool valid, bool update) {
+    // record of voter
+    uint nullifier = input[0];
+    uint[1] memory newOwnerUint = [uint256(uint160(newOwner))];
+    require(input[1] == l.hasher.poseidon(newOwnerUint), "Wrong owner");
+
+    if(!l.recover_nullifier_set.contains(nullifier)) {
+      // proof is not replay
+      if(l.socialRecoveryVerifier.verifyProof(a, b, c, input)) {
+        // proof is valid
+        l.cur_vote += 1;
+        if(l.cur_vote >= l.vote_threshold) {
+          // Threshold reached
+          l.cur_vote = 0;
+
+          for(uint i = 0; i< l.recover_nullifier_set.length(); i++) {
+            (uint n, ) = l.recover_nullifier_set.at(i);
+            l.recover_nullifier_set.remove(n);
+          }
+
+          // update merkle root
+          l.root = input[2];
+          (valid, update) = (true, true);
+        } else {
+          // Threshold not reached
+          l.recover_nullifier_set.set(nullifier, 1);
+          (valid, update) = (true, false);
+        }
+      } else {
+        // proof is not valid
+        (valid, update) = (false, false);
+      }
+    } else {
+      // proof is replay
+      (valid, update) = (false, false);
+    }
+  }
+
+  function getGuardians(GuardianStorage.Layout storage l) external view returns (uint[] memory) {
+    uint[] memory gs = new uint[](l.num_guardians);
+    for(uint i = 0; i < l.num_guardians; i++) {
+      gs[i] = l.guardians[i];
+    }
+    return gs;
+  }
+}
---
+++ packages/account-abstraction/contracts/samples/PrivateRecoveryAccount.sol
@@ -0,0 +1,232 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+/* solhint-disable avoid-low-level-calls */
+/* solhint-disable no-inline-assembly */
+/* solhint-disable reason-string */
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+import "./core/BaseAccount.sol";
+import "./GuardianStorage.sol";
+
+/**
+  * minimal account.
+  *  this is sample minimal account.
+  *  has execute, eth handling methods
+  *  has a single signer that can send requests through the entryPoint.
+  */
+contract PrivateRecoveryAccount is BaseAccount, UUPSUpgradeable, Initializable {
+    using ECDSA for bytes32;
+    using GuardianStorage for GuardianStorage.Layout;
+
+    //filler member, to push the nonce and owner to the same slot
+    // the "Initializeble" class takes 2 bytes in the first slot
+    bytes28 private _filler;
+
+    //explicit sizes of nonce, to fit a single storage cell with "owner"
+    uint96 private _nonce;
+    address public owner;
+    address public pendingOwner;
+
+    IEntryPoint private immutable _entryPoint;
+
+    event PrivateRecoveryAccountInitialized(IEntryPoint indexed entryPoint, address indexed owner);
+
+    modifier onlyOwner() {
+        _onlyOwner();
+        _;
+    }
+
+    /// @inheritdoc BaseAccount
+    function nonce() public view virtual override returns (uint256) {
+        return _nonce;
+    }
+
+    /// @inheritdoc BaseAccount
+    function entryPoint() public view virtual override returns (IEntryPoint) {
+        return _entryPoint;
+    }
+
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+
+    constructor(IEntryPoint anEntryPoint) {
+        _entryPoint = anEntryPoint;
+        _disableInitializers();
+    }
+
+    function _onlyOwner() internal view {
+        //directly from EOA owner, or through the account itself (which gets redirected through execute())
+        require(msg.sender == owner || msg.sender == address(this), "only owner");
+    }
+
+    /**
+     * execute a transaction (called directly from owner, or by entryPoint)
+     */
+    function execute(address dest, uint256 value, bytes calldata func) external {
+        _requireFromEntryPointOrOwner();
+        _call(dest, value, func);
+    }
+
+    /**
+     * execute a sequence of transactions
+     */
+    function executeBatch(address[] calldata dest, bytes[] calldata func) external {
+        _requireFromEntryPointOrOwner();
+        require(dest.length == func.length, "wrong array lengths");
+        for (uint256 i = 0; i < dest.length; i++) {
+            _call(dest[i], 0, func[i]);
+        }
+    }
+
+    /**
+     * @dev The _entryPoint member is immutable, to reduce gas consumption.  To upgrade EntryPoint,
+     * a new implementation of PrivateRecoveryAccount must be deployed with the new EntryPoint address, then upgrading
+      * the implementation by calling `upgradeTo()`
+     */
+    function initialize(address anOwner) public virtual initializer {
+        _initialize(anOwner);
+    }
+
+    function _initialize(address anOwner) internal virtual {
+        owner = anOwner;
+        emit PrivateRecoveryAccountInitialized(_entryPoint, owner);
+    }
+
+    // Require the function call went through EntryPoint or owner
+    function _requireFromEntryPointOrOwner() internal view {
+        require(msg.sender == address(entryPoint()) || msg.sender == owner, "account: not Owner or EntryPoint");
+    }
+
+    /// implement template method of BaseAccount
+    function _validateAndUpdateNonce(UserOperation calldata userOp) internal override {
+        require(_nonce++ == userOp.nonce, "account: invalid nonce");
+    }
+
+    /// implement template method of BaseAccount
+    function _validateSignature(UserOperation calldata userOp, bytes32 userOpHash)
+    internal override virtual returns (uint256 validationData) {
+        bytes32 hash = userOpHash.toEthSignedMessageHash();
+        if (owner != hash.recover(userOp.signature))
+            return SIG_VALIDATION_FAILED;
+        return 0;
+    }
+
+    function _call(address target, uint256 value, bytes memory data) internal {
+        (bool success, bytes memory result) = target.call{value : value}(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+
+    /**
+     * check current account deposit in the entryPoint
+     */
+    function getDeposit() public view returns (uint256) {
+        return entryPoint().balanceOf(address(this));
+    }
+
+    /**
+     * deposit more funds for this account in the entryPoint
+     */
+    function addDeposit() public payable {
+        entryPoint().depositTo{value : msg.value}(address(this));
+    }
+
+    /**
+     * withdraw value from the account's deposit
+     * @param withdrawAddress target to send to
+     * @param amount to withdraw
+     */
+    function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public onlyOwner {
+        entryPoint().withdrawTo(withdrawAddress, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal view override {
+        (newImplementation);
+        _onlyOwner();
+    }
+
+    /**
+     * get current guardians
+     */
+    function getGuardians() external view returns (uint[] memory) {
+        return GuardianStorage.layout().getGuardians();
+    }
+
+    /**
+     * initialize guardians
+     * @param guardians guardian list
+     * @param vote_threshold threshold to update owner
+     * @param root merkle root
+     * @param updateGuardianVerifierAddress update guardian contract address
+     * @param socialRecoveryVerifierAddress social recovery contract address
+     * @param poseidonContractAddress poseidon hasher contract address
+     */
+    function initilizeGuardians(
+      uint[] memory guardians,
+      uint vote_threshold,
+      uint root,
+      address updateGuardianVerifierAddress,
+      address socialRecoveryVerifierAddress,
+      address poseidonContractAddress
+    ) external onlyOwner {
+      GuardianStorage.layout().initialize(
+        guardians,
+        vote_threshold,
+        root,
+        updateGuardianVerifierAddress,
+        socialRecoveryVerifierAddress ,
+        poseidonContractAddress
+      );
+    }
+
+    /**
+     * update guardian by a proof
+     * @param a proof parameter
+     * @param b proof parameter
+     * @param c proof parameter
+     * @param input proof input
+     */
+    function updateGuardian(
+      uint[2] memory a,
+      uint[2][2] memory b,
+      uint[2] memory c,
+      uint[6] memory input
+    ) external returns (bool) {
+      return GuardianStorage.layout().updateGuardian(a, b, c, input);
+    }
+
+    /**
+     * recover owner
+     * @param newOwner new owner
+     * @param a proof parameter
+     * @param b proof parameter
+     * @param c proof parameter
+     * @param input proof input
+     */
+    function recover(
+      address newOwner,
+      uint[2] memory a,
+      uint[2][2] memory b,
+      uint[2] memory c,
+      uint[3] memory input
+    ) external returns (bool valid, bool update) {
+      require(pendingOwner == address(0) || newOwner == pendingOwner, "Wrong new owner");
+      (valid, update) = GuardianStorage.layout().recover(a, b, c, input, newOwner);
+      if(valid) {
+        if (update) {
+          owner = newOwner;
+          pendingOwner = address(0);
+        } else if (pendingOwner == address(0)) {
+            pendingOwner = newOwner;
+        }
+      }
+    }
+}
